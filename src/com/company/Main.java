3810f784ca4a11866774445fd7936a122e669da5
VincetCampos <vcamposs1@miumg.edu.gt>
2021-02-22 22:43:28 -0600

    Mi primer commit en Java

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String mensaje = "Hola Mundo desde Java";
+        System.out.println(mensaje);
+    }
+}
